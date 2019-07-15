046c936e99cf2fa9155d54ae5974b5d3fc9ee783
Ivan De Alba <ivan_dealba@outlook.com>
2019-07-14 23:38:52 -0500

    Created button to start trivia game; click event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,34 @@
-// var gameStart = true;
+let gameStart = true;
 
-// if (gameStart === true){
-//     $('#main-content').text("test");
-// }
+let clickToStart = $('<button>');
+clickToStart.text("Start Game");
+clickToStart.css('padding', '20px');
+clickToStart.css('margin', '20px');
+clickToStart.attr('id', 'button-start');
+
+const questionsArr = ['questionOne', 'questionTwo', 'questionThree'];
+
+function newGame(){
+    if(gameStart === true){
+        $('#main-content').text("");
+        $('#main-content').append(clickToStart);
+    } else{
+        // start questions
+        // call function for executing trivia questions
+        triviaQuestions();
+    }
+}
+
+newGame();
+
+$('#button-start').on('click', function(){
+    gameStart = false;
+    newGame();
+})
+
+function triviaQuestions(){
+    for(var i = 0; i < questionsArr.length; i++){
+        let currentQuestion = Math.floor(Math.random() * questionsArr.length);
+        $('#main-content').text(questionsArr[currentQuestion]);
+    }
+}
