f2a60e8e887a9a2c5de43efab80355adc2174681
Ivan De Alba <ivan_dealba@outlook.com>
2019-07-15 20:57:21 -0500

    Question incorporated to markup depending on math.random

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,60 @@
+// Toggle
 let gameStart = true;
 
+// Button to start game
 let clickToStart = $('<button>');
 clickToStart.text("Start Game");
 clickToStart.css('padding', '20px');
 clickToStart.css('margin', '20px');
 clickToStart.attr('id', 'button-start');
 
+// Counter
 var totalCorrect = 0;
 var totalWrong = 0;
 
+// Questions assigned to individual constants as strings
+const questionOne = "Who is Gon Freecss' best friend in Hunter x Hunter?";
+const questionTwo = "Who does Agumon digi-evolve into?";
+const questionThree = "What is the name of the nine-tailed beast was sealed inside Naruto Uzumaki when he was an infant?";
+const questionFour = "What is the name of the male character in the first season of Yu-Gi-Oh! who is known for having three Blue Eyes White Dragons?";
+const questionFive = "What is the name of the character in Avatar the Last Airbender who is an earth bender?";
+
+// Array holding answer choices for their respect question
+// questionOne pertains to the object at index 0, questionTwo pertains to the object at index 1, and so on
 const questionsArr = [{
-    questionOne: {
-        "Who is Gon Freecss' best friend in Hunter x Hunter?": {
-            correct: "Killua Zoldyck",
-            wrong: "Hisoka Morow",
-            wrong: "Isaac Netero",
-            wrong: "Silva Zoldyck"
-        }
+    question: {
+        correct: "Killua Zoldyck",
+        wrong: "Hisoka Morow",
+        wrong: "Isaac Netero",
+        wrong: "Silva Zoldyck"
     }
 }, {   
-    questionTwo: {
-        "Who does Agumon digi-evolve into?": {
-            correct: "Greymon",
-            wrong: "Angemon",
-            wrong: "Kabuterimon",
-            wrong: "Garurumon"
-        }
+    question: {
+        correct: "Greymon",
+        wrong: "Angemon",
+        wrong: "Kabuterimon",
+        wrong: "Garurumon"
     }
 }, {
-    questionThree: {
-        "What is the name of the nine-tailed beast was sealed inside Naruto Uzumaki when he was an infant?": {
-            correct: "Kurama",
-            wrong: "Gyuki",
-            wrong: "Kokuo",
-            wrong: "Matatabi"
-        }
+    question: {
+        correct: "Kurama",
+        wrong: "Gyuki",
+        wrong: "Kokuo",
+        wrong: "Matatabi"
     }
 }, {
-    questionFour: {
-    "What is the name of the male character in the first season of Yu-Gi-Oh! who is known for having three Blue Eyes White Dragons?": {
+    question: {
         correct: "Seto Kaiba",
         wrong: "Maximillion Pegasus",
         wrong: "Weevil Underwood",
         wrong: "Dinosaur Ryuzaki"
     }
 }, {
-    questionFive: {
-        "What is the name of the character in Avatar the Last Airbender who is an earth bender?": {
-            correct: "Toph",
-            wrong: "Sokka",
-            wrong: "Katara",
-            wrong: "Zuko"
-        }
+    question: {
+        correct: "Toph",
+        wrong: "Sokka",
+        wrong: "Katara",
+        wrong: "Zuko"
     }
 }];
 
@@ -74,8 +77,20 @@ $('#button-start').on('click', function(){
 })
 
 function triviaQuestions(){
-    for(var i = 0; i < questionsArr.length; i++){
-        let currentQuestion = Math.floor(Math.random() * questionsArr.length);
-        $('#main-content').text(questionsArr[currentQuestion]);
-    }
+    // for(var i = 0; i < questionsArr.length; i++){
+        let randomQuestion = Math.floor(Math.random() * questionsArr.length);
+        console.log(randomQuestion);
+
+        if(randomQuestion === 0){
+            $('#main-content').text(questionOne);
+        } else if(randomQuestion === 1){
+            $('#main-content').text(questionTwo);
+        } else if(randomQuestion === 2){
+            $('#main-content').text(questionThree);
+        } else if(randomQuestion === 3){
+            $('#main-content').text(questionFour);
+        } else if(randomQuestion === 4){
+            $('#main-content').text(questionFive);
+        }
+    // }
 }
